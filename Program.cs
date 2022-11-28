e211a45dd8af2772f36ab43ecfc0b650754c5439
Stanley <stas1498@icloud.com>
2022-11-28 22:52:17 +0100

    Tilllagt utkast kring Sprint Backloggen

--- Program.cs
+++ Program.cs
@@ -4,8 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, my second world");
+            // Ett simpelt TicTacToe spel presenteras här. Två spelare finns det.
+            // Som input skriver användare siffra 1 till 9.
+
+            // Spelaren vinner så fort hen fått tre kryss/nollor på rad/i kolumn alternativt på diagonalen.
+
+
+            // skapa relevanta variabler:
+            // int spelare, int valet, bool endTheGame etc.
+            // char[] rutan för att förvara värdena från spelets rutor (rutor #1 till #9, exkluderad 0)
+
+
+            // Sprint backlogg #1, spelaren1 alltid kör med X, spelaren2 - med O
+
+            // Sprint backlogg #2 TBD 
+
+            // #3 TBD ...
+
+
+            // #4 TBD 
+
+            // #5 Är input korrekt && den berörda rutan ej upptagen - sätt X eller O. Växla mellan spelarna.
+            // Kodmässigt: 
+            //
+            // if( (input == korrekt) && (rutan[position] != 'X') && (rutan[position] != 'O') ) 
+            // {
+            //      if(spelaren % 2 == 0) { rutan[position] == 'O'; spelaren++; }
+            //      else { rutan[position] == 'X'; spelaren++; }
+            // else
+            // Console.WriteLine($"ERROR! Rutan #{rutan[position]} upptagen. Välj en annan rutan!"); 
+            // }
+            
+
+
+
+
+
+
+
+
         }
     }
 }
